f3f885ee28bf9dba9160125be6404addab69b564
subscr <joaopedrosilves18@gmail.com>
2025-02-16 18:40:56 -0300

    chore: docs routes

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,9 @@ import { layout, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
   layout("./(app)/layout.tsx", [
-    route("/", "./(app)/page.tsx")
+    route("/", "./(app)/page.tsx"),
+    layout("./(app)/docs/layout.tsx", [
+      route("docs/:sid", "./(app)/docs/[sid]/page.tsx")
+    ])
   ])
 ] satisfies RouteConfig
